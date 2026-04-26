c1dd5a1e6ff24adb99da84779685899c9ed0e735
Kosei Haruyama <kosei.haruyama@icloud.com>
2026-04-26 22:31:03 +0900

    init commit

---
+++ SuperEasy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperEasy
+//
+//  Created by 春山恒誠 on 2026/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperEasy/SuperEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperEasyApp.swift
+//  SuperEasy
+//
+//  Created by 春山恒誠 on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
